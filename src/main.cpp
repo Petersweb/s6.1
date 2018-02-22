dbeb767e9ba5c2045ccb1ca3fd663675089764a4
Petersweb <32275665+Petersweb@users.noreply.github.com>
2018-02-22 09:39:32 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,46 @@ Thread worker;
 EventQueue queue ;
 
 DigitalOut red(LED_RED,1); /* initial state 1 led is off */
+DigitalOut green(LED_GREEN,0);
+DigitalOut blue(LED_BLUE,1);
 void flash_red(void) {
     while(1){
         red.write(1);
-        wait(0.5);
+        wait(0.6);
         red.write(0);
-        wait(0.5);
+        wait(0.3);
     }
 }
+void flash_green(void){
+  while(1){
+    green.write(0);
+    wait(0.3);
+    green.write(1);
+    wait(0.6);
+  }
+}
+void flash_blue(void){
+  while(1){
+    wait(0.3);
+    blue.write(1);
+    wait(0.3);
+    blue.write(0);
+    wait(0.3);
+    blue.write(1);
+  }
+}
+
+
 int main() {
 
     // put your setup code here, to run once:
     worker.start( flash_red );
-
+    worker.start( flash_green );
+    worker.start( flash_blue );
     while(1) {
         // put your main code here, to run repeatedly:
+
+
+
     }
 }
